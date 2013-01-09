02f6692b1f3ea8320159b19c75b4a716c19263c3
felixcarmona <felix46r@gmail.com>
2013-01-09 15:37:39 +0100

    tmp function whattype() thats will help to dispatch the type of memory argument

--- deephp.c
+++ deephp.c
@@ -9,7 +9,8 @@ ZEND_BEGIN_ARG_INFO_EX(func_args, 1, 0, 0)
 ZEND_END_ARG_INFO()
 
 static zend_function_entry deephp_functions[] = { // php 5.4+ zend_function_entry : else : function_entry
-PHP_FE(get_memory_address, func_args)
+PHP_FE(get_array_memory_address, func_args)
+PHP_FE(whattype, func_args)
 {NULL, NULL, NULL}
 };
 
@@ -34,7 +35,14 @@ STANDARD_MODULE_PROPERTIES
 ZEND_GET_MODULE(deephp)
 #endif
 
-PHP_FUNCTION(get_memory_address)
+
+
+
+
+
+
+
+PHP_FUNCTION(get_array_memory_address)
 {
 zval *var;
 char r[100];
@@ -43,4 +51,57 @@ RETURN_NULL();
 }
 sprintf(r, "%p", Z_ARRVAL_P(var));
 RETURN_STRING(r, 1);
+}
+
+PHP_FUNCTION(whattype)
+{
+
+    zval **arg;
+    if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "Z", &arg) == FAILURE) {
+        RETURN_NULL();
+    }
+
+    switch (Z_TYPE_PP(arg)) {
+        case IS_NULL:
+            RETVAL_STRING("NULL", 1);
+            break;
+
+        case IS_BOOL:
+            RETVAL_STRING("boolean", 1);
+            break;
+
+        case IS_LONG:
+            RETVAL_STRING("integer", 1);
+            break;
+
+        case IS_DOUBLE:
+            RETVAL_STRING("double", 1);
+            break;
+
+        case IS_STRING:
+            RETVAL_STRING("string", 1);
+            break;
+
+        case IS_ARRAY:
+            RETVAL_STRING("array", 1);
+            break;
+
+        case IS_OBJECT:
+            RETVAL_STRING("object", 1);
+            break;
+
+        case IS_RESOURCE:
+             {
+                 const char *type_name = zend_rsrc_list_get_rsrc_type(Z_LVAL_PP(arg) TSRMLS_CC);
+
+                 if (type_name) {
+                     RETVAL_STRING("resource", 1);
+                     break;
+                 }
+             }
+
+        default:
+                RETVAL_STRING("unknown type", 1);
+        }
+
 }
--- php_deephp.h
+++ php_deephp.h
@@ -4,7 +4,8 @@
 #define PHP_DEEPHP_EXT_VERSION "1.0"
 #define PHP_DEEPHP_EXT_EXTNAME "deephp"
 
-PHP_FUNCTION(get_memory_address);
+PHP_FUNCTION(get_array_memory_address);
+PHP_FUNCTION(whattype);
 
 extern zend_module_entry deephp_module_entry;
 #define phpext_deephp_ptr &deephp_module_entry
